326af7f0bfd5c4c0e1e60d18c08118b066c3aa38
HebaShaweish <hebahshaweish@gmail.com>
2025-07-23 23:12:24 +0300

    Update Code

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+const Button = ({ name, isBeam = false, containerClass }) => { 
+   // React functional component that renders a styled button with some conditional content and classes. 
+  return (
+    <button className={`btn ${containerClass}`}>
+      {isBeam && (
+        <span className="relative flex h-3 w-3">
+          <span className="btn-ping"></span>
+          <span className="btn-ping_dot"></span>
+        </span>
+      )}
+      {name}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Target.jsx
@@ -0,0 +1,30 @@
+import { useGLTF } from '@react-three/drei';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+
+const Target = (props) => {
+  const targetRef = useRef(); // play with the scene 
+  const { scene } = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
+  );
+
+useGSAP(() => {
+   gsap.to(targetRef.current.position,{
+    y:targetRef.current.position.y + 0.5,
+    duration:1.5,
+    repeat:-1,
+    yoyo: true, // up and down
+   });
+
+});
+
+  return (
+    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]}  >
+      <primitive object={scene} />
+    </mesh>   
+  );
+};
+
+export default Target;
